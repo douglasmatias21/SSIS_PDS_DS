69361ebb3646d1cb4b11b48082f68b3deb9c7a41
Douglas Matias <douglasmatias21@gmail.com>
2021-03-19 17:08:53 -0600

    g
    gg

--- CH02 Framework Code/LoggingSampleQuery.sql
+++
@@ -1,7 +0,0 @@
-select pkg.PackageName
-	,convert(time,pkglog.EndDateTime - pkglog.StartDateTime) as DurationSeconds
-	,pkglog.Status
-from dbo.PackageLog pkglog
-join dbo.PackageVersion pkgvers on pkglog.PackageVersionID=pkgvers.PackageVersionID
-join dbo.Package pkg on pkgvers.PackageID=pkg.PackageID
-order by pkglog.StartDateTime desc
